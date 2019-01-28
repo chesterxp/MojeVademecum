8deb8c01b3d4dd580d99745b4364815c6e561c1a
chesterxp <chesterxp7@wp.pl>
2019-01-28 21:19:29 +0100

    Exercises with animation

---
+++ src/js/css.js
@@ -0,0 +1,8 @@
+const buttons_list = document.querySelector('.buttons_list');
+
+buttons_list.addEventListener('click', function(e){
+    // if(e.target)
+    console.log(e);
+    console.log(e.target);
+
+})
